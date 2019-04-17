99352081fee03143f9263b2f9cac622bc17cc13b
naonao0209 <naoya.shikichi@itdcanada.ca>
2019-04-17 09:29:51 -0700

    day one

---
+++ CSharpProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+
+            Console.ReadLine();
+        }
+    }
+}
